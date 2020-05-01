b7d93da13e91f87578098460ddffa589f16fe3de
samjyi95 <samjyi95@gmail.com>
2020-05-01 16:56:24 -0700

    completed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+let db = require('./models')
 
 const app = express();
 
@@ -9,6 +10,41 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+//home route 
+
+
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then((widgets) => {
+        res.render('home', { widgets })
+    })
+    .catch((err) => {
+        console.log(error)
+        res.send(error)
+    })
+})
+
+app.post('/post', (req,res) => {
+    db.widget.create(req.body)
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch((err) => {
+        res.send(error)
+    })
+})
+
+app.delete('/widget/:id/', (req, res) => {
+    db.widget.destroy({
+        where: { id: req.params.id }
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch((err) => {
+        res.send(error)
+    })
+})
 
 
 
